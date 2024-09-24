fe4b680f7ac343bdee91eab42ceba334f94f65e7
Ivan Jimenez <ivanj3815@gmail.com>
2024-09-24 12:23:04 -0500

    Creacion del proyecto

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import SplashScreen from "./views/SplashScreen.vue"
+import { defineAsyncComponent} from "vue";
+
+const Dashboard = defineAsyncComponent(()=>{
+  return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(import("./views/Dashboard.vue"))
+      }, 3000)
+  })
+})
+
+</script>
+
+<template>
+  <Suspense>
+    <template #default>
+      <Dashboard/>
+    </template>
+    <template #fallback>
+      <SplashScreen class="splash-hide"/>
+    </template>
+  </Suspense>
+</template>
---
+++ src/views/SplashScreen.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+    <div>
+        <h1>Aéreo seguridad Colplinio</h1>
+    </div>
+</template>
