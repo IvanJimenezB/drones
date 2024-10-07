42bbd3b9bdbb3fdca749ed8a8f9d98645ae53413
Ivan Jimenez <ivanj3815@gmail.com>
2024-10-06 21:57:32 -0500

    Responsive Welcome y agregamos seccion para conocer mas del proyecto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,42 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import WelcomeView from './WelcomeView.vue';
+import { ref } from 'vue';
+ 
+const showWelcome = ref(true);
 
+setTimeout(() => {
+    showWelcome.value = false
+}, 3000);
+
+</script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <WelcomeView v-if="showWelcome"/>
+    <div class="absolute top-0 w-full h-[100vh] bg-[#0f172a] flex direction column" transition-style="in:wipe:right">
+        <div class="w-3/12 bg-white bg-opacity-10 blur-sm">
+            <nav class="text-black">
+                <RouterLink class="blur-none" to="/about">Pilotear</RouterLink>
+                <RouterLink class="blur-none" to="/about">Tipos</RouterLink>
+                <RouterLink class="blur-none" to="/about">Partes</RouterLink>
+                <RouterLink class="blur-none" to="/about">Alcance</RouterLink>
+                <RouterLink class="blur-none" to="/about">Evolucion</RouterLink>
+            </nav>
+        </div>
+        <RouterView />
+    </div>
 </template>
+<style scoped>
+@keyframes wipe-in-right {
+    from {
+        clip-path: inset(0 100% 0 0);
+    }
+
+    to {
+        clip-path: inset(0 0 0 0);
+    }
+}
+
+[transition-style="in:wipe:right"] {
+    animation: 2.5s cubic-bezier(.25, 1, .30, 1) wipe-in-right both;
+}
+</style>
